1ca029328d77caec63128d890f3a6a9a659aa0af
Mikhail Lagutik <lma358829@gmail.com>
2023-03-04 13:59:29 +0300

    03-objects-arrays(2-invert-object)

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,16 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+    if (obj === undefined){
+        return undefined
+    }
+    let result = {};
+    let sol = Object.entries(obj);
+    for (const [key, value] of sol){
+        if (value === `undefined`){
+            return result
+        }
+        result[value] = key;
+    };
+    return result
 }
