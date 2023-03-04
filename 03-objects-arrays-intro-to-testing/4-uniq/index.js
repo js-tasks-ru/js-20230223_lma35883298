db35a92a74ef919f1f5a17fed6bd4b916a8e02db
Mikhail Lagutik <lma358829@gmail.com>
2023-03-04 18:43:02 +0300

    [200~

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -3,6 +3,16 @@
  * @param {*[]} arr - the array of primitive values
  * @returns {*[]} - the new array with uniq values
  */
-export function uniq(arr) {
 
+export function uniq(arr) {
+    let result = [];
+    if (arr === [] || arr === undefined){
+        return result
+    };
+    arr.forEach(element => {
+        if (!result.includes(element)){
+            result.push(element);
+        }
+    });
+    return result
 }
