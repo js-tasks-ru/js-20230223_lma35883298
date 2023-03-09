b0384da6292626a5299570ca656ed3824c71046f
Mikhail Lagutik <lma3588329@gmail.com>
2023-03-09 19:20:22 +0300

    03-objects-arrays (create-getter)

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,15 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+    const pathArr = path.split('.');
 
-}
+    return obj =>{
+        let result = obj;
+
+        for (const item of pathArr){
+            if (result === undefined) return;
+            result = result[item];    
+        }
+        return result;    
+    };
+};
