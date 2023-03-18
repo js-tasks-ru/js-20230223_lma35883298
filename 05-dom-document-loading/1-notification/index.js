d6dbc26e9dec5951819dbee03b0f48e4531deb56
Mikhail Lagutik <lma358829@gmail.com>
2023-03-18 14:38:51 +0300

    05-dom-document

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,67 @@
 export default class NotificationMessage {
+    static activeNotification;
+    
+    element;
+    timerId;
 
+    constructor(message = '', {
+        duration = 2000,
+        type = 'success',
+      } = {}) {
+        this.message = message;
+        this.durationInSeconds = (duration / 1000) + 's';
+        this.type = type;
+        this.duration = duration;
+    
+        this.render();
+      }
+
+    get template(){
+        return/*html*/`
+        <div class="notification ${this.type}" style="--value:${this.durationInSeconds}">
+            <div class="timer"></div>
+                <div class="inner-wrapper">
+                <div class="notification-header">${this.type}</div>
+                <div class="notification-body">
+                    ${this.message}
+                </div>
+            </div>
+        </div>`
+    }
+
+    render(){
+        const element = document.createElement('div');
+
+        element.innerHTML = this.template;
+
+        this.element = element.firstElementChild;
+    }
+
+    show(place = document.body){
+        if (NotificationMessage.activeNotification){
+            NotificationMessage.activeNotification.remove()
+        }
+
+        place.append(this.element);
+
+        this.timerId = setTimeout(() => {
+            this.remove();
+        }, this.duration);
+        
+        NotificationMessage.activeNotification = this;
+    }
+
+    remove(){
+        clearTimeout(this.timerId);
+
+        if (this.element){
+            this.element.remove();
+        }
+    }
+
+    destroy(){
+        this.remove();
+        this.element = null;
+        NotificationMessage.activeNotification = null;
+    }
 }
