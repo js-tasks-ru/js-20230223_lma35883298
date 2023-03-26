021e9b155a33b0b798f6f5aff78c982d482e042f
Mikhail-07 <125125622+Mikhail-07@users.noreply.github.com>
2023-03-26 15:06:09 +0300

    1-column-chart FETCH
    Решение 1-column-chart

--- 07-async-code-fetch-api-part-1/1-column-chart/index.js
+++ 07-async-code-fetch-api-part-1/1-column-chart/index.js
@@ -3,5 +3,127 @@ import fetchJson from './utils/fetch-json.js';
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
 export default class ColumnChart {
+    subElements = {};
+    chartHeight = 50;
 
+    constructor ({
+        label = '', 
+        link = '', 
+        formatHeading = data => data,
+        url = '',
+        range = {
+            from: new Date(),
+            to: new Date (),}
+        } = {}){
+
+        this.url = new URL(url, BACKEND_URL);
+        this.range = range;
+        this.label = label;
+        this.link = link;
+
+        this.formatHeading = formatHeading;
+
+        this.render();
+        this.update(this.range.from, this.range.to);
+        }
+    
+        setNewRange(from, to) {
+            this.range.from = from;
+            this.range.to = to;
+          }   
+        
+    get template() {
+        return `
+            <div class="column-chart column-chart_loading" style="--chart-height: ${this.chartHeight}">
+            <div class="column-chart__title">
+                Total ${this.label}
+                ${this.getLink()}
+            </div>
+            <div class="column-chart__container">
+                <div data-element="header" class="column-chart__header"></div>
+                <div data-element="body" class="column-chart__chart"></div>
+            </div>
+            </div>
+        `;
+        }
+
+    getColumnHeight(data) {
+        const maxValue = Math.max(...Object.values(data));
+        
+        return Object.entries(data).map(([key, value]) => {
+            const scale = this.chartHeight / maxValue;
+            const percent = (value / maxValue * 100).toFixed(0);
+            const tooltip = `<span>
+              <small>${key.toLocaleString('default', {dateStyle: 'medium'})}</small>
+              <br>
+              <strong>${percent}%</strong>
+            </span>`;
+      
+            return `<div style="--value: ${Math.floor(value * scale)}" data-tooltip="${tooltip}"></div>`;
+          }).join('');
+        }
+
+    getLink() {
+        return this.link
+          ? `<a class="column-chart__link" href="${this.link}">View all</a>`
+          : "";
+      }
+
+    render(){
+        const element = document.createElement('div');
+
+        element.innerHTML = this.template;
+
+        this.element = element.firstElementChild;
+
+        this.subElements = this.getSubElements(this.element);
+    }
+
+    getHeaderValue(data) {
+        return this.formatHeading(Object.values(data).reduce((accum, item) => (accum + item), 0));
+      }
+
+    getSubElements(){
+        const result = {};
+        const elements = this.element.querySelectorAll("[data-element]");
+
+        for (const item of elements){
+            const name = item.dataset.element;
+            result[name] = item;
+        }
+        return result
+    }
+
+    async update (from, to){
+        this.element.classList.add('column-chart_loading');
+
+        const data = await this.loadData(from, to);
+
+        this.setNewRange (from, to);
+        if (data && Object.values(data).length){
+            this.subElements.header.textContent = this.getHeaderValue (data);
+            this.subElements.body.innerHTML = this.getColumnHeight(data);
+            this.element.classList.remove('column-chart_loading')
+        }
+        this.data = data;
+        return this.data;
+    }
+
+    async loadData(from, to){
+        this.url.searchParams.set('from', from.toISOString());  
+        this.url.searchParams.set('to', to.toISOString());
+
+        return await fetchJson(this.url)
+    }
+
+    
+    remove() {
+        this.element.remove();
+    }
+
+    destroy() {
+        this.remove();
+        this.element = {};
+        this.subElements = {}
+    }
 }
